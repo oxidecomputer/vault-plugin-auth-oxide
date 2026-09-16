10ed1450dddbfe8163932efa027652bd1e94d3ef
Josh Carp <jm.carp@gmail.com>
2026-09-16 11:12:21 -0400

    Move plugin logic to top-level package.

---
+++ backend.go
@@ -0,0 +1,51 @@
+package oxideauth
+
+import (
+	"context"
+
+	"github.com/openbao/openbao/sdk/v2/framework"
+	"github.com/openbao/openbao/sdk/v2/helper/locksutil"
+	"github.com/openbao/openbao/sdk/v2/logical"
+)
+
+func Factory(ctx context.Context, c *logical.BackendConfig) (logical.Backend, error) {
+	b := Backend(c)
+	if err := b.Setup(ctx, c); err != nil {
+		return nil, err
+	}
+	b.locks = locksutil.CreateLocks()
+	return b, nil
+}
+
+type backend struct {
+	*framework.Backend
+
+	locks []*locksutil.LockEntry
+}
+
+const backendHelp = "The Oxide plugin backend allows Oxide instances to authenticate to Vault using instance attestation."
+
+func Backend(_ *logical.BackendConfig) *backend {
+	var b backend
+
+	b.Backend = &framework.Backend{
+		Help:        backendHelp,
+		BackendType: logical.TypeCredential,
+		Paths: []*framework.Path{
+			b.pathVerifier(),
+			b.pathListVerifier(),
+			b.pathRole(),
+			b.pathListRole(),
+			b.pathAuthNonce(),
+			b.pathAuthLogin(),
+		},
+		PathsSpecial: &logical.Paths{
+			Unauthenticated: []string{
+				"login",
+				"nonce",
+			},
+		},
+	}
+
+	return &b
+}
--- cmd/vault-plugin-auth-oxide/main.go
+++ cmd/vault-plugin-auth-oxide/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"context"
 	"log"
 	"os"
 
 	"github.com/openbao/openbao/api/v2"
-	"github.com/openbao/openbao/sdk/v2/framework"
-	"github.com/openbao/openbao/sdk/v2/helper/locksutil"
-	"github.com/openbao/openbao/sdk/v2/logical"
 	"github.com/openbao/openbao/sdk/v2/plugin"
+	oxideauth "github.com/oxidecomputer/vault-plugin-auth-oxide"
 )
 
 func main() {
@@ -24,51 +21,9 @@ func main() {
 	tlsProviderFunc := api.VaultPluginTLSProvider(tlsConfig)
 
 	if err := plugin.ServeMultiplex(&plugin.ServeOpts{
-		BackendFactoryFunc: Factory,
+		BackendFactoryFunc: oxideauth.Factory,
 		TLSProviderFunc:    tlsProviderFunc,
 	}); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func Factory(ctx context.Context, c *logical.BackendConfig) (logical.Backend, error) {
-	b := Backend(c)
-	if err := b.Setup(ctx, c); err != nil {
-		return nil, err
-	}
-	b.locks = locksutil.CreateLocks()
-	return b, nil
-}
-
-type backend struct {
-	*framework.Backend
-
-	locks []*locksutil.LockEntry
-}
-
-const backendHelp = "The Oxide plugin backend allows Oxide instances to authenticate to Vault using instance attestation."
-
-func Backend(_ *logical.BackendConfig) *backend {
-	var b backend
-
-	b.Backend = &framework.Backend{
-		Help:        backendHelp,
-		BackendType: logical.TypeCredential,
-		Paths: []*framework.Path{
-			b.pathVerifier(),
-			b.pathListVerifier(),
-			b.pathRole(),
-			b.pathListRole(),
-			b.pathAuthNonce(),
-			b.pathAuthLogin(),
-		},
-		PathsSpecial: &logical.Paths{
-			Unauthenticated: []string{
-				"login",
-				"nonce",
-			},
-		},
-	}
-
-	return &b
 }
